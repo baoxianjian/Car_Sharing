b604a1ae406ab66796258423f0a7c4abba3b28ac
baoxianjian <baoxianjian@gmail.com>
2017-09-28 11:25:38 +0800

    delete

--- sql/alterUser.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `carsharingdb`.`users` 
-CHANGE COLUMN `city` `city` VARCHAR(50) CHARACTER SET 'utf8' NULL ,
-CHANGE COLUMN `street` `street` VARCHAR(50) CHARACTER SET 'utf8' NULL ,
-CHANGE COLUMN `birthyear` `birthyear` INT(4) UNSIGNED NULL ;
